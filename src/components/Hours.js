80f5c5573eb2e9862ac404229ee59133c8d45a16
JesusFerru <90222631+JesusFerru@users.noreply.github.com>
2024-08-28 05:08:15 +0000

    Add hours component in the project

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,23 @@
-// Placeholder to host a component
+// components/Hours.js
+import React from 'react';
+
+const Hours = () => {
+  const today = new Date();
+  const day = today.getDay(); // 0 (domingo) - 6 (sábado)
+  
+  let hours;
+  if (day >= 1 && day <= 5) { // Lunes a viernes
+    hours = "10 a.m. - 4 p.m.";
+  } else { // Sábado y domingo
+    hours = "9 a.m. - 8 p.m.";
+  }
+
+  return (
+    <div>
+      <h2>Horario de atención:</h2>
+      <p>Hoy: {hours}</p>
+    </div>
+  );
+};
+
+export default Hours;
